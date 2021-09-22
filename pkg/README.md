c6c3dafb14155de5307ce3b24c0b6ca7e9e790d2
Hugo Ledoux <h.ledoux@tudelft.nl>
2021-09-22 12:00:12 +0200

    Add /pkg/*

---
+++ pkg/README.md
@@ -0,0 +1,13 @@
+# cjval_wasm
+
+## To compile and run locally:
+
+1. install [wasm-pack](https://rustwasm.github.io/wasm-pack/installer/)
+2. `wasm-pack build -t web --out-dir ./www/pkg/`
+3. `cd ./www/`
+4. `python -m http.server 8080` (or any local webserver will do)
+5. with your browser go to http://localhost:8080/
+
+## Online demo
+
+[==> demo](https://hugoledoux.github.io/cjval_wasm/)
---
+++ pkg/cjval2_wasm_bg.wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_validator_free(a: number): void;
+export function validator_from_str(a: number, b: number): number;
+export function validator_validate_schema(a: number): number;
+export function validator_parent_children_consistency(a: number): number;
+export function validator_wrong_vertex_index(a: number): number;
+export function validator_duplicate_vertices(a: number): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
