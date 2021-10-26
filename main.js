abc4223a0fa5ef0f85264370dd5153afe9e12381
Hugo Ledoux <h.ledoux@tudelft.nl>
2021-10-26 09:27:20 +0200

    bulk of www folder

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-import init, { Validator } from './pkg/cjval2_wasm.js';
+import init, { Validator } from './pkg/cjval_wasm.js';
 
 
-const allerrors = ["err_json", "err_schema", "err_wrong_vertex_index", "err_parent_child", "war_dup_vertices"];
+const allerrors = ["err_json_syntax", 
+                   "err_schema", 
+                   "err_ext_schema", 
+                   "err_parents_children_consistency",
+                   "err_parents_children_consistency", 
+                   "err_wrong_vertex_index",
+                   "err_semantics_arrays", 
+                   "war_duplicate_vertices",
+                   "war_unused_vertices",
+                   "war_extra_root_properties"
+                  ];
+
+
 
 async function main() {
   console.log("init");
@@ -64,163 +76,262 @@ async function main() {
   }
 }
 
-function reset_results(){
-  $("#tab-errors").hide(); 
-  for (let i = 0; i < allerrors.length; i++) {
-    let e = document.getElementById(allerrors[i]);
-    e.classList.remove("table-success", "table-danger", "table-warning");
-    e.children[1].innerHTML = "";
-  }
-  $("#result-success").hide();
-  $("#result-warning").hide();
-  $("#result-error").hide();
-}
-
-function display_final_result(filename, isValid, hasWarnings) {
-  $("#tab-errors").show();
-  $("#tab-warnings").show();
-  if (isValid) {
-    if (!hasWarnings) {
-      document.getElementById("result-success").children[0].innerHTML = filename;
-      $("#result-success").show();
-    } else {
-      document.getElementById('result-warning').children[0].innerHTML = filename;
-      $("#result-warning").show();
-    }
-  } else {
-    document.getElementById('result-error').children[0].innerHTML = filename;
-    $("#result-error").show();   
-  }
-}
-
 //-- executed when files are uploaded
 async function handleFiles(files) {
   if (files[0] == null) {
     return;
   }
   reset_results();
-
-  // console.log("yeah I validate here {}", files);
-
-  var isValid = true;
-  var hasWarnings = false;
-  var f = files[0];
+  var f = files[0]; //-- read only the first file
   var reader = new FileReader();
   reader.readAsText(f);
+  console.log(f);
+  document.getElementById('inputsummary').children[0].innerHTML = f.name;
+  document.getElementById('inputsummary').classList.remove('invisible');
   reader.onload = function() {
     let validator;
     try {
       validator = Validator.from_str(reader.result);
-      document.getElementById('err_json').className = "table-success";
+      document.getElementById('err_json_syntax').className = "table-success";
+      let cjv = validator.get_input_cityjson_version();
+      console.log(cjv);
+      if (cjv == 11){
+        document.getElementById('cjversion').innerHTML = "v1.1";
+      } else if (cjv == 10) {
+        document.getElementById('cjversion').innerHTML = "v1.0 (it would be a good idea to upgrade to v1.1)";
+      } else {
+        document.getElementById('cjversion').innerHTML = "version <1.0 (no validation possible)";
+      }
     } catch (error) {
       console.log(error);
-      document.getElementById('err_json').className = "table-danger";
-      document.getElementById('err_json').children[1].innerHTML = error;
+      document.getElementById('err_json_syntax').className = "table-danger";
+      document.getElementById('err_json_syntax').children[1].innerHTML = error;
       isValid = false;
-      display_final_result(f.name, isValid, hasWarnings);
+      display_final_result(isValid, hasWarnings);
       return;
     }
-    let re = validator.validate_schema();
-    if (re == null) {
-      document.getElementById('err_schema').className = "table-success";
-    } else {
-      document.getElementById('err_schema').className = "table-danger";
-      document.getElementById('err_schema').children[1].innerHTML = re;
-      isValid = false;
-      display_final_result(f.name, isValid, hasWarnings);
-      return;
-    }
-    re = validator.wrong_vertex_index();
-    if (re == null) {
-      document.getElementById('err_wrong_vertex_index').className = "table-success";
-    } else {
-      document.getElementById('err_wrong_vertex_index').className = "table-danger";
-      document.getElementById('err_wrong_vertex_index').children[1].innerHTML = re;
-      isValid = false;
-    }
-    re = validator.parent_children_consistency();
-    if (re == null) {
-      document.getElementById('err_parent_child').className = "table-success";
-    } else {
-      document.getElementById('err_parent_child').className = "table-danger";
-      document.getElementById('err_parent_child').children[1].innerHTML = re;
-      isValid = false;
-    }
-    if (isValid == false) {
-      console.log("falseeeee")
-      display_final_result(f.name, isValid, hasWarnings);
-      return;
-    }
-    //-- WARNINGS
-    re = validator.duplicate_vertices();
-    if (re == null) {
-      document.getElementById('war_dup_vertices').className = "table-success";
-    } else {
-      document.getElementById('war_dup_vertices').className = "table-warning";
-      document.getElementById('war_dup_vertices').children[1].innerHTML = re;
-      hasWarnings = true;
-    }
-    display_final_result(f.name, isValid, hasWarnings);
-    // console.log(re);
+    //-- fetch all extensions 
+    download_all_extensions(validator, () => {
+      allvalidations(validator, f.name);
+    });
+    
   }
   $("#fileElem").val("")
 }
 
+function reset_results(){
+  const myNode = document.getElementById("theextensions");
+  myNode.innerHTML = '';
+  document.getElementById("theresult").classList.remove("bg-success"); 
+  document.getElementById("theresult").classList.remove("bg-warning"); 
+  document.getElementById("theresult").classList.remove("bg-danger"); 
+  document.getElementById("theresult").classList.add('invisible');
+  for (let i = 0; i < allerrors.length; i++) {
+    let e = document.getElementById(allerrors[i]);
+    e.classList.remove("table-success", "table-danger", "table-warning");
+    e.children[1].innerHTML = "";
+  }
+}
 
-  // for (var i = 0; i < files.length; i++) {
-  //   //if file is not json
-  //   var split_file_name = files[i].name.split(".");
-  //   if (split_file_name[split_file_name.length - 1] != "json") {
-  //     alert("file '" + files[i].name + "' is not a json file");
-  //     continue
-  //   }
+function display_final_result(isValid, hasWarnings) {
+  // $("#tab-errors").show();
+  document.getElementById("tab-errors").classList.remove('invisible');
+  if (isValid) {
+    if (!hasWarnings) {
+      document.getElementById("theresult").innerHTML = "The file is 100% valid!";
+      document.getElementById("theresult").classList.add("bg-success");
 
-  //   //if file already exist
-  //   if (files[i].name.split(".")[0] in jsonDict) {
-  //     alert("file '" + files[i].name + "' already loaded!");
-  //     continue
-  //   }
+    } else {
+      document.getElementById("theresult").innerHTML = "The file is valid but has warnings";
+      document.getElementById("theresult").classList.add("bg-warning");
+    }
+  } else {
+      document.getElementById("theresult").innerHTML = "The file is invalid";
+      document.getElementById("theresult").classList.add("bg-danger");
+  }
+  document.getElementById("theresult").classList.remove('invisible');
+}
 
-  //   //load json into memory
-  //   var objectURL = window.URL.createObjectURL(files[i])
-  //   var json = await loadJSON(objectURL)
-  //   var jsonName = files[i].name.split(".")[0]
 
-  //   //json file has an error and cannot be loaded
-  //   if (json == -1){
-  //     window.alert("File " + jsonName + ".json has an error and cannot be loaded!")
-  //     continue
-  //   }
+function download_all_extensions(val, _callback) {
+  let re = val.has_extensions();
+  if (re != null) {
+    let urls = re.split('\n');
+    // const promises = [];
+    // for (let i = 0; i < urls.length; i++) {
+    //   let a = 
+    //     fetch(urls[i])
+    //     .then(y => y.text())
+    //     .catch((error) => {
+    //       console.error('Error:', error);
+    //     }
+    //   );
+    //   promises.push(a);
+    // }
+    var promises = urls.map(url => 
+      fetch(url)
+      .then(y => y.text())
+      .catch((error) => {
+        console.error('Error:', error);
+        alert("Error: cannot dowload Extension (Cross-Origin Request Blocked)");
+      })
+    );
+    console.log(promises);
+    Promise.all(promises).then(results => {
+      for (let i = 0; i < results.length; i++) {
+        if (results[i] == "404: Not Found") {
+          console.log("404:", urls[i]);
+          const li = document.createElement("li");
+          li.classList.add("list-group-item");
+          li.classList.add("d-flex");
+          li.classList.add("justify-content-between");
+          li.classList.add("align-items-center");
+          li.innerHTML = urls[i];
+          const sp = document.createElement("span");
+          sp.classList.add("badge");
+          sp.classList.add("bg-danger");
+          sp.classList.add("rounded-pill");
+          sp.innerHTML = "error";
+          li.appendChild(sp);
+          document.getElementById("theextensions").appendChild(li);
+          display_final_result(false, false);
+          return;
+        } else {
+          const li = document.createElement("li");
+          li.classList.add("list-group-item");
+          li.classList.add("d-flex");
+          li.classList.add("justify-content-between");
+          li.classList.add("align-items-center");
+          li.innerHTML = urls[i];
+          const sp = document.createElement("span");
+          sp.classList.add("badge");
+          sp.classList.add("bg-success");
+          sp.classList.add("rounded-pill");
+          sp.innerHTML = "ok";
+          li.appendChild(sp);
+          document.getElementById("theextensions").appendChild(li);
+          val.add_one_extension_from_str(urls[i], results[i]);
+        }
+      }
+      _callback();
+    });
+  }
+  else {
+    const li = document.createElement("li");
+    li.classList.add("list-group-item");
+    li.classList.add("d-flex");
+    li.classList.add("justify-content-between");
+    li.classList.add("align-items-center");
+    li.innerHTML = "none";
+    document.getElementById("theextensions").appendChild(li);
+    _callback();
+  }
+}
 
-  //   //add json to the dict
-  //   jsonDict[jsonName] = json;
 
-  //   //add it to the infoBox
-  //   $("#filesBox").show();
-  //   $('#TreeView').append('<li id="li_' + jsonName + '">' +
-  //     '<span onclick="toggleTree(this);" id="span_' + jsonName + '">▽</span>' +
-  //     '<input type="checkbox" onclick="toggleFile(this);" id="checkFile_' + jsonName + '" checked>' +
-  //     '<span class="spanLiFileName">' + jsonName + '</span>' +
-  //     '<ul class="fileTree" id="ul_' + jsonName + '"></ul>' +
-  //     '</li>')
+function allvalidations(validator, fname) {
+  console.log("all validations");
+  console.log("# exts1: ", validator.get_extensions());
+  var isValid = true;
+  var hasWarnings = false;
+  //-- validate_schema
+  let re = validator.validate_schema();
+  if (re == null) {
+    document.getElementById('err_schema').className = "table-success";
+  } else {
+    document.getElementById('err_schema').className = "table-danger";
+    document.getElementById('err_schema').children[1].innerHTML = re;
+    isValid = false;
+    display_final_result(isValid, hasWarnings);
+    return;
+  }
 
-  //   //load the cityObjects into the viewer
-  //   await loadCityObjects(jsonName)
+  if (validator.get_input_cityjson_version == 11) {
+    re = validator.validate_extensions();
+    if (re == null) {
+      document.getElementById('err_ext_schema').className = "table-success";
+    } else {
+      document.getElementById('err_ext_schema').className = "table-danger";
+      document.getElementById('err_ext_schema').children[1].innerHTML = re;
+      isValid = false;
+      display_final_result(isValid, hasWarnings);
+      return;
+    }
+  } else {
+    if (validator.get_extensions() > 0) {
+      document.getElementById('err_ext_schema').className = "table-danger";
+      document.getElementById('err_ext_schema').children[1].innerHTML = "validation of Extensions is not supported in v1.0, upgrade to v1.1";
+      isValid = false;
+      display_final_result(isValid, hasWarnings);
+      return;
+    }
+  }
 
-  //   //already render loaded objects
-  //   renderer.render(scene, camera);
-  //   console.log("JSON file '" + jsonName + "' loaded")
+  //-- wrong vertex index
+  re = validator.wrong_vertex_index();
+  if (re == null) {
+    document.getElementById('err_wrong_vertex_index').className = "table-success";
+  } else {
+    document.getElementById('err_wrong_vertex_index').className = "table-danger";
+    document.getElementById('err_wrong_vertex_index').children[1].innerHTML = re;
+    isValid = false;
+  }
+  re = validator.parent_children_consistency();
+  if (re == null) {
+    document.getElementById('err_parents_children_consistency').className = "table-success";
+  } else {
+    document.getElementById('err_parents_children_consistency').className = "table-danger";
+    document.getElementById('err_parents_children_consistency').children[1].innerHTML = re;
+    isValid = false;
+  }
+  re = validator.semantics_arrays();
+  if (re == null) {
+    document.getElementById('err_semantics_arrays').className = "table-success";
+  } else {
+    document.getElementById('err_semantics_arrays').className = "table-danger";
+    document.getElementById('err_semantics_arrays').children[1].innerHTML = re;
+    isValid = false;
+  }
 
-  // }
+  if (isValid == false) {
+    // console.log("falseeeee")
+    display_final_result(isValid, hasWarnings);
+    return;
+  }
+  //-- WARNINGS
+  re = validator.duplicate_vertices();
+  if (re == null) {
+    document.getElementById('war_duplicate_vertices').className = "table-success";
+  } else {
+    document.getElementById('war_duplicate_vertices').className = "table-warning";
+    document.getElementById('war_duplicate_vertices').children[1].innerHTML = re;
+    hasWarnings = true;
+  }
+  re = validator.extra_root_properties();
+  if (re == null) {
+    document.getElementById('war_extra_root_properties').className = "table-success";
+  } else {
+    document.getElementById('war_extra_root_properties').className = "table-warning";
+    document.getElementById('war_extra_root_properties').children[1].innerHTML = re;
+    hasWarnings = true;
+  }    
+  re = validator.unused_vertices();
+  if (re == null) {
+    document.getElementById('war_unused_vertices').className = "table-success";
+  } else {
+    document.getElementById('war_unused_vertices').className = "table-warning";
+    document.getElementById('war_unused_vertices').children[1].innerHTML = re;
+    hasWarnings = true;
+  }     
+  //-- FINAL RESULTS
+  display_final_result(isValid, hasWarnings);
+  console.log(re);  
+}
 
-  // //hide loader when loadin is finished
-  // $("#loader").hide();
 
-  // //global variable that a json is loaded
-  // boolJSONload = true
 
-  // //reset the input
-  // $("#fileElem").val("")
+
+
 
 
 main();
